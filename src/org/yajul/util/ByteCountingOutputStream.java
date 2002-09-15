3d412388a3470b3a6bca4e159ceb946dec606148
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2002-09-15 22:29:03 +0000

    Fix up build.xml and add the new files.

---
+++ src/org/yajul/util/ByteCountingOutputStream.java
@@ -0,0 +1,92 @@
+/*********************************************************************************
+ *   $Header$
+ * $Workfile: ByteCountingOutputStream.java $
+ * $Revision$
+ *  $Modtime: 10/23/00 7:22p $
+ *   $Author$
+ *
+ *               Copyright 1999-2000 metiom, inc.               
+ *********************************************************************************/
+/*********************************************************************************
+*      $Log$
+*      Revision 1.1  2002/09/15 22:29:03  pgmjsd
+*      Fix up build.xml and add the new files.
+*
+*      Revision 1.2  2000/12/01 23:55:33  cvsuser
+*      First cut.
+*      date	2000.10.23.23.27.00;	author joshuad;	state Exp;
+*
+* 
+* 2     10/23/00 7:27p Joshuad
+* First cut.
+**********************************************************************************/
+
+package org.yajul.util;
+
+import java.io.OutputStream;
+import java.io.IOException;
+
+/**
+ * Output stream wraper that counts the number of bytes.
+ */
+public class ByteCountingOutputStream extends OutputStream
+{
+    private int m_bytes;
+    private OutputStream m_stream;
+    
+    /**
+     * Creates a new output stream that counts then number of bytes written to 
+     * the output stream specified.
+     */
+    public ByteCountingOutputStream(OutputStream out)
+    {
+        m_bytes = 0;
+        m_stream = out;
+    }
+    
+    /**
+     * Closes this output stream and releases any system resources associated with this stream. 
+     */
+    public void close()
+        throws IOException
+    {
+        m_stream.close();
+        m_stream = null;
+    }
+    
+    /**
+     * Writes b.length bytes from the specified byte array to this output stream. 
+     */
+    public void write(byte[] b)
+        throws IOException
+    {
+        m_stream.write(b);
+        m_bytes += b.length;
+    }
+    
+    /**
+     * Writes len bytes from the specified byte array starting at offset off to this output stream. 
+     */
+    public void write(byte[] b, int off, int len) 
+        throws IOException
+    {
+        m_stream.write(b,off,len);
+        m_bytes += len;
+    }
+    
+    /**
+    * Writes the specified byte to this output stream. 
+    */
+    public  void write(int b)
+        throws IOException
+    {
+        m_stream.write(b);
+        m_bytes++;
+    }
+    
+    /**
+     * Returns the number of bytes written.
+     */
+    public int getByteCount() { return m_bytes; }
+    
+}
---
+++ src/org/yajul/util/TeeOutputStream.java
@@ -0,0 +1,116 @@
+/*********************************************************************************
+ *   $Header$
+ * $Workfile: TeeOutputStream.java $
+ * $Revision$
+ *  $Modtime: 10/30/00 12:58p $
+ *   $Author$
+ *********************************************************************************/
+/*********************************************************************************
+*      $Log$
+*      Revision 1.1  2002/09/15 22:29:03  pgmjsd
+*      Fix up build.xml and add the new files.
+*
+*      Revision 1.2  2000/12/02 00:00:47  cvsuser
+*      Added VSS header.
+*      date	2000.10.31.22.38.00;	author joshuad;	state Exp;
+*
+* 
+* 2     10/31/00 5:38p Joshuad
+* Added VSS header.
+**********************************************************************************/
+
+package org.yajul.util;
+
+import java.io.OutputStream;
+import java.io.IOException;
+
+/**
+ * Output stream wrapper that echoes all characters written to the other
+ * output streams.
+ */
+public class TeeOutputStream extends OutputStream
+{
+    private OutputStream[] m_stream;    // Array of streams to write to.
+    
+    /**
+     * Creates a new output stream that echoes output to both
+     * of the specified streams in the constructor.
+     */
+    public TeeOutputStream(OutputStream out,OutputStream out2)
+    {
+        m_stream = new OutputStream[2];
+        m_stream[0] = out;
+        m_stream[1] = out2;
+    }
+    
+    /**
+     * Closes this output stream and releases any system resources associated with this stream. 
+     */
+    public void close()
+        throws IOException
+    {
+        IOException e = null;
+        
+        for (int i = 0; i < m_stream.length ; i++)
+        {
+            try { m_stream[i].close(); } catch (IOException ioe) { e = ioe; }
+        }
+        m_stream = null;
+        if (e != null)
+            throw e;
+    }
+    
+    /**
+     * Writes b.length bytes from the specified byte array to this output stream. 
+     */
+    public void write(byte[] b)
+        throws IOException
+    {
+        IOException e = null;
+        
+        for (int i = 0; i < m_stream.length ; i++)
+        {
+            try {
+                m_stream[i].write(b);
+            } catch (IOException ioe) { e = ioe; }
+        }
+        if (e != null)
+            throw e;
+    }
+    
+    /**
+     * Writes len bytes from the specified byte array starting at offset off to this output stream. 
+     */
+    public void write(byte[] b, int off, int len) 
+        throws IOException
+    {
+        IOException e = null;
+        
+        for (int i = 0; i < m_stream.length ; i++)
+        {
+            try {
+                m_stream[i].write(b,off,len);
+            } catch (IOException ioe) { e = ioe; }
+        }
+        if (e != null)
+            throw e;
+    }
+    
+    /**
+    * Writes the specified byte to this output stream. 
+    */
+    public  void write(int b)
+        throws IOException
+    {
+        IOException e = null;
+        
+        for (int i = 0; i < m_stream.length ; i++)
+        {
+            try {
+                m_stream[i].write(b);
+            } catch (IOException ioe) { e = ioe; }
+        }
+        if (e != null)
+            throw e;
+    }
+}
