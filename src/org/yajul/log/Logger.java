a3a4c966d58688a6a6fa1f797b34d2bc8ed64fcb
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2002-09-15 14:02:08 +0000

    Cleanup junk out of IOUtil.  Add priority level and get root to Logger.

--- src/org/yajul/log/Logger.java
+++ src/org/yajul/log/Logger.java
@@ -23,58 +23,54 @@ public class Logger extends Writer
 {
     /** The Log4J layout for the default logging configuration. */
     public static final String LAYOUT = "%-6r [%8t] %-5p %15.15c{1} - %m\n";
-    
+
+    /** Logging level for 'debug' messages. */
+    public static final int LEVEL_DEBUG = 4;
+
+    /** Logging level for 'info' messages. */
+    public static final int LEVEL_INFO = 3;
+
+    /** Logging level for 'warning' messages. */
+    public static final int LEVEL_WARNING = 2;
+
+    /** Logging level for 'error' messages. */
+    public static final int LEVEL_ERROR = 1;
+
+    /** Logging level for 'fatal' messages. */
+    public static final int LEVEL_FATAL = 0;
+
     /** The delegate log object. */
     private Category log;
+
     /** A print writer that is piped to this log. */
     private PrintWriter printWriter;
-    
+
     /** Flag that indicates whether Log4J has been initialized or not. */
     private static boolean configured = false;
 
-    /**
-     * Returns an instance of a logger class.
-     * @param c - The class that wants to log.
-     */
-    public static Logger getLogger(Class c)
-    {
-        return getLogger(c.getName());
-    }
-    
-    /**
-     * Returns an instance of a logger, given the category name. 
-     * @param categoryName - The name of the category.
-     */
-    public static synchronized Logger getLogger(String categoryName)
-    {
-        if (!configured)
-        {
-            PatternLayout layout = new PatternLayout(LAYOUT);
-            ConsoleAppender console = new ConsoleAppender(layout);
-            BasicConfigurator.configure(console);
-            configured = true;
-        }
-        return new Logger(categoryName);
-    }
-    
+    /** An array of Log4J priorities to make decoding the LEVEL_xxx values easier. */
+    private static Priority[] priorities =
+        { Priority.FATAL, Priority.ERROR, Priority.WARN, Priority.INFO, Priority.DEBUG };
+
+
     /**
      * Creates a new logger with a delegate that logs to the Log4J category
      * 'categoryName'
-     * @param catagoryName - The name of the category.
+     * @param catagoryName      The name of the category.
      */
     private Logger(String categoryName)
     {
         log = Category.getInstance(categoryName);
         printWriter = new PrintWriter(this);
     }
-    
+
     // --- java.io.Writer implementation ---
-    
+
     /**
      * java.io.Writer implementation - Write the characters to the log.
-     * @param buf - The array of characters to write.
-     * @param off - The offset in array where the writable characters start.
-     * @param len - The number of characters to write.
+     * @param buf       The array of characters to write.
+     * @param off       The offset in array where the writable characters start.
+     * @param len       The number of characters to write.
      */
     public void write(char[] buf,int off,int len)
     {
@@ -82,36 +78,86 @@ public class Logger extends Writer
         String str = new String(buf,off,len);
         log.info(str);
     }
-    
+
     /**
      * java.io.Writer implementation - Flush any buffered output.
      */
     public void flush()
     {
     }
- 
+
     /**
      * java.io.Writer implementation - Close the writer.
      */
     public void close()
     {
     }
-    
+
+    // -- End of Writer implementation --
+
+    // --- JDK1.4 compatible methods ---
+
+    /**
+     * Returns the 'root' logger, to which *all* messages are sent.
+     * @return Logger       The root logger.
+     */
+    public static Logger getRootLogger()
+    {
+        return internalGetLogger(null); // Null indicates the root.
+    }
+
+    /**
+     * Returns an instance of a logger, given the category name.
+     * @param categoryName      The name of the category.
+     * @return Logger           The logger for the specified category.
+     */
+    public static Logger getLogger(String categoryName)
+    {
+        if (categoryName == null || categoryName.length() == 0)
+            throw new IllegalArgumentException(
+                "A logging category name was not specified.  Specify a name, or call Logger.getRootLogger()");
+        return internalGetLogger(categoryName);
+    }
+
+    /**
+     * Returns an instance of a logger, given the category class.  The name of the category
+     * will be the name of the class
+     * @param categoryClass     The class of the category.
+     * @return Logger           The logger for the specified category.
+     */
+    public static Logger getLogger(Class categoryClass)
+    {
+        return getLogger((categoryClass != null) ? categoryClass.getName() : null);
+    }
+
+    /** Returns true if DEBUG level is enabled for this logger.
+     * @return boolean      True if 'debug' is enabled.*/
+    public boolean isDebugEnabled()
+    {
+        Priority p = Priority.DEBUG;
+        if(! log.isEnabledFor(p) )
+            return false;
+        return p.isGreaterOrEqual(log.getChainedPriority());
+    }
+
+    /**
+     * Set the logging level for the logger,  messages sent to this logger that
+     * are *less than* this category will not be shown.
+     * @param level - The level (LEVEL_xxx) for this log.
+     */
+    public void setLevel(int level)
+    {
+        if (level < 0 || level >= priorities.length)
+            throw new IllegalArgumentException("The supplied logging level was not valid: " + level);
+        log.setPriority(priorities[level]);
+    }
+
     /**
      * Returns this logger as if it was a print writer.
      */
     public PrintWriter getPrintWriter()
     {
         return printWriter;
-    }
-    
-    /** Returns true if TRACE level is enabled for this logger. */
-    public boolean isDebugEnabled()
-    {
-        Priority p = Priority.DEBUG;
-        if( log.isEnabledFor(p) == false )
-            return false;
-        return p.isGreaterOrEqual(log.getChainedPriority());
     }
 
     /** Log the message with a level of DEBUG. */
@@ -140,7 +186,7 @@ public class Logger extends Writer
     {
         log.log(Priority.INFO, message);
     }
-    
+
     /** Log the message and the exception with a level of INFO. */
     public void info(Object message, Throwable t)
     {
@@ -152,7 +198,7 @@ public class Logger extends Writer
     {
         log.log(Priority.WARN, message);
     }
-        
+
     /** Log the message and the exception with a level of WARN. */
     public void warn(Object message, Throwable t)
     {
@@ -164,8 +210,8 @@ public class Logger extends Writer
     {
         log.log(Priority.ERROR, message);
     }
-    
-    /** Log the message and the exception with a level of ERROR. */    
+
+    /** Log the message and the exception with a level of ERROR. */
     public void error(Object message, Throwable t)
     {
         log.log(Priority.ERROR, message, t);
@@ -176,13 +222,13 @@ public class Logger extends Writer
     {
         log.log(Priority.FATAL, message);
     }
-    
+
     /** Log the message and the exception with a level of FATAL. */
     public void fatal(Object message, Throwable t)
     {
         log.log(Priority.FATAL, message, t);
     }
-    
+
     /**
      * Log a standard 'unexpected exception' message.
      * @param throwable - An exception.
@@ -190,5 +236,32 @@ public class Logger extends Writer
     public void unexpected(Throwable throwable)
     {
         error("Unexpected exception: " + throwable.getMessage(), throwable);
+    }
+
+    // --- Implementaiton methods ---
+
+    /**
+     * Internal: Returns a logger for the category name, or the root if the category name is null.
+     * Also configures Log4J, if it has not already been configured.
+     * @param categoryName - The category requested.
+     * @return Logger - The requested logger.
+     */
+    private static Logger internalGetLogger(String categoryName)
+    {
+        if (!configured)    // If Log4J has not been configured, set up a default configuration.
+        {
+            synchronized (Logger.class)  // Synchronize thread on the class and check the configuraiton flag again...
+            {
+                if (!configured)
+                {
+                    PatternLayout layout = new PatternLayout(LAYOUT);
+                    ConsoleAppender console = new ConsoleAppender(layout);
+                    BasicConfigurator.configure(console);
+                    configured = true;
+//                  UNUSED:  loglogger.setPriority(Priority.INFO);
+                } // if
+            } // synchronized
+        } // if
+        return new Logger(categoryName);
     }
 }
--- src/org/yajul/util/IOUtil.java
+++ src/org/yajul/util/IOUtil.java
@@ -1,23 +1,18 @@
-
 package org.yajul.util;
 
 // JDK
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.IOException;
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FilenameFilter;
-import java.io.PrintStream;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.Set;
 
 import org.yajul.log.Logger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.StringTokenizer;
 
 /**
  * Provides static methods that implement commonly used IO functions.
@@ -28,9 +23,10 @@ public class IOUtil
 {
     /** The default buffer size for the copy() method. **/
     public static final int DEFAULT_BUFFER_SIZE = 256;
-    
+
     /** The file separator string ("/" on UNIX) */
     public static final String FILESEP = System.getProperty("file.separator");
+
     /** A logger for this class. */
     private static Logger log = Logger.getLogger(IOUtil.class.getName());
 
@@ -41,8 +37,8 @@ public class IOUtil
      * @param bufsz - The size of the buffer to use.
      * @throws IOException - When the stream could not be copied.
      **/
-    public static final void copy(InputStream in,OutputStream out,int bufsz)
-        throws IOException
+    public static final void copy(InputStream in, OutputStream out, int bufsz)
+            throws IOException
     {
         // From Java I/O, page 43
         // Do not allow other threads to read from the input or write to the
@@ -58,24 +54,24 @@ public class IOUtil
                     bytesRead = in.read(buf);
                     if (bytesRead == -1)
                         break;
-                    out.write(buf,0,bytesRead);
+                    out.write(buf, 0, bytesRead);
                 } // while
             } // synchronized (out)
         } // synchronized (in)
     }
-    
+
     /**
      * Copies the input stream into the output stream in a thread safe and efficient manner.
      * @param in - The input stream.
      * @param out - The output stream.
      * @throws IOException - When the stream could not be copied.
      **/
-    public static final void copy(InputStream in,OutputStream out)
-        throws IOException
+    public static final void copy(InputStream in, OutputStream out)
+            throws IOException
     {
-        copy(in,out,DEFAULT_BUFFER_SIZE);
+        copy(in, out, DEFAULT_BUFFER_SIZE);
     }
-    
+
     /**
      * Reads the input stream into an array of bytes.
      * @param in - The input stream.
@@ -85,20 +81,20 @@ public class IOUtil
     public static final byte[] toByteArray(InputStream in) throws IOException
     {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        copy(in,baos);
+        copy(in, baos);
         return baos.toByteArray();
     }
-    
+
     /**
-     * make directory, making sure that the whole path is created
+     * Make directory, making sure that the whole path is created
      * @param path The path to be created
      */
-    public static synchronized void makeDir(String path)
+    public static final void makeDir(String path)
     {
         StringTokenizer tok = new StringTokenizer(path, FILESEP);
         String dir = FILESEP;
 
-        while(tok.hasMoreTokens())
+        while (tok.hasMoreTokens())
         {
             dir = dir + tok.nextToken() + FILESEP;
             File f = new File(dir);
@@ -108,37 +104,37 @@ public class IOUtil
     }
 
     /**
-     * Deletes only the files (not the subdirectories) from a directory, 
+     * Deletes only the files (not the subdirectories) from a directory,
      * or creates it if it does not exist.  Be very <b>CAREFUL</b> when using this
-     * method, since it does not ask for any confirmation before deleting 
+     * method, since it does not ask for any confirmation before deleting
      * all the files in the given directory.
      * @param path the path to the directory
      */
-    public static synchronized void clearDir(String path)
+    public static final void clearDir(String path)
     {
         File dir = new File(path);
 
         if (dir.exists())
         {
-            //empty it out
+            // empty it out
             if (log.isDebugEnabled())
                 log.debug("Deleting files from directory " + path);
             File[] files = dir.listFiles();
-            for (int i = 0; i < files.length; i ++)
+            for (int i = 0; i < files.length; i++)
                 files[i].delete();
         }
         else
         {
             //make it
-            makeDir (path);
+            makeDir(path);
         }
-    }    
+    }
 
     /** Returns an array of strings representing all the subdirectories in the directory
      * @param d the directory
      * @return the list of the subdirectories
      */
-    public static synchronized String[] listSubDirectories(String d)
+    public static final String[] listSubDirectories(String d)
     {
         File dir = new File(d);
         if (!dir.isDirectory())
@@ -148,17 +144,17 @@ public class IOUtil
         }
         // return the filtered entries
         return dir.list(
-            new FilenameFilter()
-            {
-                public boolean accept(File directory, String name)
+                new FilenameFilter()
                 {
-                    return (new File(directory, name)).isDirectory();
-                }
-            } // anonymous inner class
+                    public boolean accept(File directory, String name)
+                    {
+                        return (new File(directory, name)).isDirectory();
+                    }
+                } // anonymous inner class
         );
     }
 
-    /** 
+    /**
      * Perfom preliminary opeation for writing to a file
      * @param path Directory name.
      * @param name File name (without directory)
@@ -166,7 +162,7 @@ public class IOUtil
      *        else, if false, the existing file will be deleted
      * @return the PrintStream object ready for printing
      */
-    public static synchronized PrintStream getPrintStream(String path, String name, boolean append)
+    public static final PrintStream getPrintStream(String path, String name, boolean append)
     {
         if (!path.endsWith(FILESEP))
             path = path + FILESEP;
@@ -177,7 +173,7 @@ public class IOUtil
         return getPrintStream(path + name.trim(), append);
     }
 
-    /** 
+    /**
      * Perfom preliminary opeation for writing to a file
      * @param fullName File name (including path)
      * @param append if true, the file will be open for appending,
@@ -185,12 +181,12 @@ public class IOUtil
      *
      * @return the PrintStream object ready for printing
      */
-    public static synchronized PrintStream getPrintStream(String fullName, boolean append)
+    public static final PrintStream getPrintStream(String fullName, boolean append)
     {
         File file = new File(fullName.trim());
         if (!append && file.exists())
         {
-            log.warn("Erasing existing file: " +  fullName);
+            log.warn("Erasing existing file: " + fullName);
             file.delete();
         }
 
@@ -201,7 +197,7 @@ public class IOUtil
         }
         catch (IOException e1)
         {
-            log.error("Problem writing to file: " + fullName,e1);
+            log.error("Problem writing to file: " + fullName, e1);
             return null;
         }
         return output;
@@ -223,130 +219,15 @@ public class IOUtil
                 return null;
             if (file.getParentFile() == null)
                 return null;
-            return  file.getCanonicalPath().substring(
-                file.getCanonicalFile().getParentFile().getCanonicalPath().length() + 1);
-                
+            return file.getCanonicalPath().substring(
+                    file.getCanonicalFile().getParentFile().getCanonicalPath().length() + 1);
+
         }
-        catch(IOException ioe)
+        catch (IOException ioe)
         {
             // Well, this isn't supposed to happen, so return null.
             return null;
         }
-    }        
-
-
-    /** Return an array of strings representing all the files with a given extension in a directory
-     *  (ex: ".xml", ".doc")
-     *@param d the directory
-     *@param extension usually the 3-letter extension of a file (ex: "xml" or "doc") - should not include the dot "."
-     *@return the list of the names of files ending in the given extension (ex: ".xml", ".doc")
-     */
-    public static synchronized String[] getFilesWithExtension(String d, final String extension)
-    {
-        File dir = new File(d);
-
-        if (!dir.isDirectory())
-        {
-            log.warn(d + " is not a directory");
-            return new String[0];
-        }
-
-        if (extension == null)
-        {
-            log.warn("extension is null");
-            return new String[0];
-        }
-
-        // return the filtered entries
-        return dir.list(new FilenameFilter() 
-            {
-                public boolean accept(File f, String name) 
-                {
-                    return name.endsWith("." + extension);}
-            }
-                        );
-
     }
-
-    /** Reads in a file which contains instrument names (one per line) 
-     * and returns them as an ArrayList of strings
-     * @param file name of the file that contains instrument names
-     * @return ArrayList constaining instrument names  as Strings
-     */
-    public static ArrayList readNamesFromFile(String file)
-    {
-        return readNamesFromFile(file, null, null);
-    }
-
-    /** Reads in a file which contains (instrument) names with priorities and returns them as an array of strings.  The file
-     * can contain comments (ex: lines starting with # for now) and each instrument must have a priority
-     * number associated with the individual instrument if the priority is not null.  The priority is the last
-     * character on the line, a number from 1 to 9 separated by a space from the name.
-     * @param file name of the file that contains instrument names
-     * @param priority the priority used to determine if a instruments is to be selected or not.  null
-     *        means don't use (and don't expect) priority.  1 is lowest priority and it would include all
-     *        names.  a priority of 5 would return only the names that have a priority written as 5 or lower.
-     * @param commentChars a set of characters that denote the beginning of a line with comments (ex: "#" -
-     *        for perl-style comments)
-     * @return ArrayList constaining instrument names  as Strings
-     */
-    public static synchronized ArrayList readNamesFromFile (String file, Integer priority, Set commentChars)
-    {
-        BufferedReader br = null;
-        try
-        {
-            br = new BufferedReader(new FileReader(file));
-        }
-        catch(FileNotFoundException e)
-        {
-            log.error("Cannot find file:" + e.getMessage());
-            return null;
-        }
-        ArrayList list = new ArrayList();
-        try
-        {
-            for (String input = br.readLine(); input != null; input = br.readLine())
-            {
-                //get rid of spaces at both ends
-                String line = input.trim();
-                //skip empty lines and comments. comments must be on lines that start with 
-                //known comment chars, passed in the commentsChar set (ex: "#")
-                if ( (line.length() == 0) || 
-                     ((commentChars != null) && (commentChars.contains(line.substring(0,0)))))
-                    continue;
-
-                //got a line that has a valid entry. 
-                //if priority is null, just add it; else, check the priority
-                if (priority == null)
-                {
-                    list.add(line);
-                }
-                else
-                {
-                    //the priority is the last
-                    //character on the line, a number from 1 to 9
-                    //separated by a space from the name 
-                    int readPriority = line.charAt(line.length() - 1) - '0';
-                    if ((readPriority < 1) || (readPriority > 9))
-                    {
-                        log.error("Incorrect priority on line: " + line);
-                        return null;
-                    }
-                    else if (readPriority >= priority.intValue())
-                    {
-                        //got correct entry, adding it, without the last 2 chars
-                        list.add(line.substring(0, line.length() - 2));
-                    }
-                }
-            }
-        }
-        catch (IOException e)
-        {
-            log.error("Error while reading " + file);
-            return null;
-        }
-        return list;
-    }
-
 }
 
