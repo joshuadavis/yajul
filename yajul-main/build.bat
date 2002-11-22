f14ceae34b4742ca048aa5c3e1ce1ec99958908d
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2002-11-22 03:49:48 +0000

    New version of centipede

--- yajul-main/build.bat
+++ yajul-main/build.bat
@@ -1,14 +1,8 @@
 @echo off
-
-set OLD_ANT_HOME=%ANT_HOME%
-set ANT_HOME=tools\antipede
-
-set OLD_CLASSPATH=%CLASSPATH%
-set CLASSPATH=
-for %%i in (.\lib\endorsed\*.jar) do call appendcp.bat %%i
-
-echo Using classpath: "%CLASSPATH%"
-call %ANT_HOME%\bin\ant -emacs -logger org.apache.tools.ant.NoBannerLogger %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-set ANT_HOME=%OLD_ANT_HOME%
-set CLASSPATH=%OLD_CLASSPATH%
+if "%CENTIPEDE_HOME%"=="" goto no_centipede
+%CENTIPEDE_HOME%\bin\cent.bat
+goto exit
+:no_centipede
+echo You must set CENTIPEDE_HOME to point at your Centipede installation
+:exit
+  
--- yajul-main/build.sh
+++ yajul-main/build.sh
@@ -6,8 +6,10 @@ if [ "$JAVA_HOME" = "" ] ; then
   exit 1
 fi
 
-chmod u+x ./tools/antipede/bin/antRun
-chmod u+x ./tools/antipede/bin/ant
+if [ "$CENTIPEDE_HOME" = "" ] ; then
+  echo You must set CENTIPEDE_HOME to point at your Centipede installation
+  exit 1
+fi
 
 # ----- Verify and Set Required Environment Variables -------------------------
 
@@ -17,24 +19,4 @@ else
   S=':'
 fi
 
-# ----- Set Up The Runtime Classpath ------------------------------------------
-
-OLD_ANT_HOME=$ANT_HOME
-unset ANT_HOME
-
-CP=$CLASSPATH
-export CP
-unset CLASSPATH
-
-CLASSPATH="`echo ./lib/endorsed/*.jar | tr ' ' $S`"
-export CLASSPATH
-
-echo Using classpath: \"$CLASSPATH\"
-$PWD/./tools/antipede/bin/ant -emacs -logger org.apache.tools.ant.NoBannerLogger $@ 
-
-unset CLASSPATH
-
-CLASSPATH=$CP
-export CLASSPATH
-ANT_HOME=OLD_ANT_HOME
-export ANT_HOME
+$CENTIPEDE_HOME/bin/cent
