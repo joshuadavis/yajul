ea79e14c8a1f1e827f23da7026ee620ba5bd28e4
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2004-01-24 18:07:11 +0000

    Added throughput statistics.

--- yajul-core/src/org/yajul/io/ByteCountingInputStream.java
+++ yajul-core/src/org/yajul/io/ByteCountingInputStream.java
@@ -43,11 +43,15 @@ import java.io.IOException;
 public class ByteCountingInputStream extends FilterInputStream
 {
     private int count;
+    private long startTime;
+    private long bytesSinceStartTime;
 
     public ByteCountingInputStream(InputStream is)
     {
         super(is);
         count = 0;
+        bytesSinceStartTime = 0;
+        startTime = 0;
     }
 
     /**
@@ -58,7 +62,7 @@ public class ByteCountingInputStream extends FilterInputStream
     {
         int b = super.read();
         if (b != -1)     // If the value isn't the EOF signal...
-            count++;    // increment the number of bytes read.
+            increment(1);
         return b;
     }
 
@@ -82,8 +86,8 @@ public class ByteCountingInputStream extends FilterInputStream
     public int read(byte b[], int off, int len) throws IOException
     {
         int bytes = super.in.read(b, off, len);
-        if (bytes != -1)     // If the value isn't the EOF signal...
-            count += bytes;  // increment the number of bytes read.
+        if (bytes != -1)        // If the value isn't the EOF signal...
+            increment(bytes);   // increment the number of bytes read.
         return bytes;
     }
 
@@ -94,15 +98,65 @@ public class ByteCountingInputStream extends FilterInputStream
             throws java.io.IOException
     {
         long b = super.in.skip(n);
-        count += b;
+        increment(b);
         return b;
     }
 
     /**
      * Returns the number of bytes read from the input stream.
+     * @return the number of bytes read from the input stream.
      */
     public int getByteCount()
     {
         return count;
+    }
+
+    /**
+     * Reset the throughput statistics.
+     */
+    public void resetStatistics()
+    {
+        startTime = System.currentTimeMillis();
+        bytesSinceStartTime = 0;
+    }
+
+    /**
+     * Returns the elapsed time in milliseconds since the first byte was read,
+     * or since the resetStatistics() method was called.
+     * @return the elapsed time in milliseconds since the first byte was read,
+     * or since the resetStatistics() method was called.
+     */
+    public long getElapsedTime()
+    {
+        if (startTime == 0)
+            return 0;
+        else
+            return System.currentTimeMillis() - startTime;
+    }
+
+    /**
+     * Returns the average throughput in bytes per second.
+     * @return the average throughput in bytes per second.
+     */
+    public double getAverageThroughput()
+    {
+        long elapsedTime = getElapsedTime();
+        if (elapsedTime <= 0)
+            return bytesSinceStartTime;
+        else
+            return ( ((double)bytesSinceStartTime) / ((double)elapsedTime) )
+                    * 1000.0;
+    }
+
+    /**
+     * Increment the counters.
+     * @param i
+     */
+    private void increment(long i)
+    {
+        count += i;
+        bytesSinceStartTime += i;
+        if (startTime == 0)
+            startTime = System.currentTimeMillis();
     }
 }
