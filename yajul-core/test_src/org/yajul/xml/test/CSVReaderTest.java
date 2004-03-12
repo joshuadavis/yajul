15134de28bae205ee1104b6e658e60199270d62c
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2004-03-12 01:27:25 +0000

    Added CSVReader

--- yajul-core/test_src/org/yajul/xml/test/CSVReaderTest.java
+++ yajul-core/test_src/org/yajul/xml/test/CSVReaderTest.java
@@ -5,10 +5,8 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 import org.yajul.xml.CSVReader;
 import org.yajul.xml.DOMUtil;
-import org.yajul.xml.DOMPrinter;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
