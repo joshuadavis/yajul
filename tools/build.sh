329162b7313e0e36981e4141698bbfb88c67ba5d
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2004-07-14 12:12:22 +0000

    Fix main build - clover should work now

--- tools/build.sh
+++ tools/build.sh
@@ -10,8 +10,11 @@ else
   S=':'
 fi
 ANT_HOME=`dirname "$0"`/ant
+# Use a sub-shell to find the absolute path for ANT_HOME.
+ANT_HOME=`(cd $ANT_HOME 2> /dev/null && pwd;)`
 echo ANT_HOME=$ANT_HOME
+echo JAVA_HOME=$JAVA_HOME
 # Launch ANT from the tools/ant/lib directory.
 ANT_CLASSPATH="-classpath $ANT_HOME/lib/ant-launcher.jar"
-ANT_OPTS="-Dant.home=$ANT_HOME -Djava.awt.headless=true"
+ANT_OPTS="-Djava.home=$JAVA_HOME -Dant.home=$ANT_HOME -Djava.awt.headless=true"
 java $ANT_CLASSPATH $ANT_OPTS org.apache.tools.ant.launch.Launcher $@
