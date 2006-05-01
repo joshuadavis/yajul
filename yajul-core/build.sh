e91cf28c65fda70f6aebe8cb5f9f88dcecec60cd
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2006-05-01 15:37:29 +0000

    add idea project, update build files

--- yajul-core/build.sh
+++ yajul-core/build.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-java -cp "../tools/ant/lib/ant-launcher.jar:$JAVA_HOME/lib/tools.jar" -Dant.home=../tools/ant org.apache.tools.ant.launch.Launcher $@
+DIR=`dirname $0`
+if [[ -z "$JAVA_HOME" ]] ; then
+	echo "ERROR: JAVA_HOME is not defined!"
+fi
+if [[ -z "$ANT_HOME" ]] ; then
+	echo "ERROR: ANT_HOME is not defined!"
+fi
+$ANT_HOME/bin/ant $@
