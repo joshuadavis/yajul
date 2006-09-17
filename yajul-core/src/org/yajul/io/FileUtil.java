5e8677337c94a9f876d69dd6449cde7a85a3d94d
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2006-09-17 15:50:31 +0000

    added 'tail' and a test

--- yajul-core/src/org/yajul/io/FileUtil.java
+++ yajul-core/src/org/yajul/io/FileUtil.java
@@ -1,18 +1,25 @@
 // $Id$
 package org.yajul.io;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Provides utility methods for dealing with files (java.io.File).
- * 
+ *
  * @author josh May 4, 2004 7:08:23 AM
  */
 public class FileUtil
 {
+    private static final int ONE_K = 1024;
+    private static final int MINIMUM_TAIL_BUFFER = 3 * ONE_K;
+    private static final int BYTES_PER_LINE = 80;
+
     /**
      * Deletes files or directories recursively, returns the number of files deleted.
+     *
      * @param f The file or directory to delete.
      * @return The number of files deleted.
      * @throws IOException if something goes wrong.
@@ -38,5 +45,42 @@ public class FileUtil
         }
         else
             throw new IOException("File is not a file or a directory!");
+    }
+
+    /**
+     * Like UN*X tail - writes the last 'n' lines of a file to the writer.
+     *
+     * @param numberOfLines the number of tail lines
+     * @param file          the file to tail
+     * @param writer        where to send the output
+     * @return long - The position of the last byte read.
+     */
+    public static long tail(int numberOfLines, File file, Writer writer) throws IOException
+    {
+        // Calculate the number of bytes to skip.  Assume that each line has about 80 chars.
+        long size = file.length();
+        long tailbuffer = numberOfLines * BYTES_PER_LINE + ONE_K;
+        long skipBytes = Math.max(size - tailbuffer, MINIMUM_TAIL_BUFFER);
+        // Note: LineNumberReader extends buffered reader, so we don't need to
+        // make a buffer.
+        LineNumberReader reader = new LineNumberReader(new FileReader(file));
+        reader.skip(skipBytes);
+        // Read in lines, only keep the last 'n' lines.
+        String line;
+        List list = new LinkedList();
+        while ((line = reader.readLine()) != null)
+        {
+            list.add(line);
+            if (list.size() > numberOfLines)
+                list.remove(0);
+        }
+        // Write out all the lines to the writer.
+        PrintWriter pw = new PrintWriter(writer);
+        for (Iterator iterator = list.iterator(); iterator.hasNext();)
+        {
+            line = (String) iterator.next();
+            pw.println(line);
+        }
+        return size;
     }
 }
