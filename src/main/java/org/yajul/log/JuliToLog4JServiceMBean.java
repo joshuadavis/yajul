b449c3687742f5f9f2584f680849cddfe673a6dd
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2008-06-05 13:57:04 +0000

    add juli to log4j handler and JMX mbean

---
+++ src/main/java/org/yajul/log/JuliToLog4JServiceMBean.java
@@ -0,0 +1,24 @@
+package org.yajul.log;
+
+import org.yajul.jmx.Lifecycle;
+
+/**
+ * JMX Interface for the Juli->Log4J MBean.
+ * <br>
+ * User: josh
+ * Date: Jun 5, 2008
+ * Time: 8:44:16 AM
+ */
+public interface JuliToLog4JServiceMBean extends Lifecycle {
+
+    /**
+     * @return The logging level of the java.util.logging->Log4J 'Handler' as a string
+     */
+    String getHandlerLevel();
+
+    /**
+     * Sets the logging level of the java.util.logging->Log4J 'Handler' to the specified value.
+     * @param level the logging level for the adapter, e.g. "INFO"
+     */
+    void setHandlerLevel(String level);
+}
