e5a1f46ef9e0f73601cbcfa05a2743cbfef42021
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2009-05-28 18:52:33 +0000

    * more decoder test work * Add qfj acceptance examples

--- fix/src/test/java/org/yajul/fix/test/FrameDecoderTest.java
+++ fix/src/test/java/org/yajul/fix/test/FrameDecoderTest.java
@@ -1,27 +1,23 @@
 package org.yajul.fix.test;
 
 import junit.framework.TestCase;
-import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.*;
-import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static org.yajul.fix.netty.ChannelBufferHelper.buffer;
 import static org.yajul.fix.netty.ChannelBufferHelper.indexOf;
 import org.yajul.fix.netty.FixFrameDecoder;
-import org.yajul.fix.netty.FixHandler;
 import org.yajul.fix.netty.ChannelBufferHelper;
 import static org.yajul.fix.util.Bytes.getBytes;
+import org.yajul.fix.RawFixMessage;
 import org.jmock.Mockery;
 import org.jmock.Expectations;
+import org.jmock.Sequence;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.Description;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.util.concurrent.Executors;
 import java.util.Arrays;
 
 /**
@@ -34,7 +30,7 @@ import java.util.Arrays;
 public class FrameDecoderTest extends TestCase {
     private final static Logger log = LoggerFactory.getLogger(FrameDecoderTest.class);
 
-    private Mockery context = new Mockery();
+    private Mockery mockery = new Mockery();
 
     public void testBufferHelper() {
         ChannelBuffer buf = buffer("8=FIX.4.2\0019=12\00135=X\001108=30\00110=049\001");
@@ -72,20 +68,21 @@ public class FrameDecoderTest extends TestCase {
     public void testDecoder() throws Exception {
         // Create the decoder, mock out all the rest.
         FixFrameDecoder decoder = new FixFrameDecoder();
-        final ChannelHandlerContext ctx = context.mock(ChannelHandlerContext.class);
-        final ChannelStateEvent cse = context.mock(ChannelStateEvent.class);
+        final ChannelHandlerContext ctx = mockery.mock(ChannelHandlerContext.class);
+        final ChannelStateEvent cse = mockery.mock(ChannelStateEvent.class);
         final ChannelBuffer buf = ChannelBuffers.dynamicBuffer(32);
-        final MessageEvent e = context.mock(MessageEvent.class);
+        final MessageEvent e = mockery.mock(MessageEvent.class);
+        final Sequence sequence = mockery.sequence("seq");
 
-        context.checking(new Expectations() { {
-            ignoring(ctx);  // We don't care about ctx.
+        mockery.checking(new Expectations() { {
+            // ignoring(ctx);  // We don't care about ctx.
             allowing(e).getChannel();   // We don't care about the channel.
             allowing(e).getRemoteAddress(); // We don't care about the remote address.
             // Three calls, return the buffer every time.  We'll change the state of buf
             // in the test.
-            one(e).getMessage(); will(returnValue(buf));
-            one(e).getMessage(); will(returnValue(buf));
-            one(e).getMessage(); will(returnValue(buf)); 
+            oneOf(ctx).sendUpstream(cse); inSequence(sequence);
+            one(e).getMessage(); will(returnValue(buf)); inSequence(sequence);
+            one(e).getMessage(); will(returnValue(buf)); inSequence(sequence);
         } });
         
         decoder.channelConnected(ctx,cse);
@@ -101,13 +98,34 @@ public class FrameDecoderTest extends TestCase {
         buf.writeBytes(getBytes("IX.4.2\0019"));
         decoder.messageReceived(ctx,e);
 
+        mockery.assertIsSatisfied();
+
         if (log.isDebugEnabled())
            log.debug("third fragment");
+
+        mockery.checking(new Expectations() { {
+            allowing(e).getChannel();   // We don't care about the channel.
+            allowing(e).getRemoteAddress(); // We don't care about the remote address.
+            one(e).getMessage(); will(returnValue(buf));
+            one(ctx).sendUpstream(with(new DefaultMessageEventMatcher()));
+            one(ctx).sendUpstream(cse);
+        } });
+        
         buf.writeBytes(getBytes("=12\00135=X\001108=30\00110=049\001"));
+
         decoder.messageReceived(ctx,e);
 
         decoder.channelDisconnected(ctx,cse);
 
-        context.assertIsSatisfied();
+        mockery.assertIsSatisfied();
+    }
+
+    class DefaultMessageEventMatcher extends TypeSafeMatcher<DefaultMessageEvent> {
+        @Override
+        public boolean matchesSafely(DefaultMessageEvent e) {
+            return e.getMessage() instanceof RawFixMessage;
+        }
+        public void describeTo(Description description) {
+        }
     }
 }
