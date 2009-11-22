97f86d466fac03df03f52c18312185f55bb2a7c3
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2009-11-22 16:36:49 +0000

    embedded jboss externals and env

--- embedded-jboss/bin/env.sh
+++ embedded-jboss/bin/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JBOSSAS_BASE=~/svn/jboss-embedded
+export JBOSSAS_BASE=~/devel/yajul/sandbox/embedded-jboss
 export JAVA_HOME=/opt/jdk1.6.0_12
 export M2_HOME=/opt/apache-maven-2.0.9
 export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$PATH
---
+++ embedded-jboss/bin/mvn-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export JAVA_HOME=/opt/jdk1.6.0_17
+export M2_HOME=/opt/apache-maven-2.0.10
+export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$PATH
