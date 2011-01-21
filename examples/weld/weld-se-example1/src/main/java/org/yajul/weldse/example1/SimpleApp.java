910beed0508c10aaacc57d2db18081dd13824261
pgmjsd <pgmjsd@1e4b57da-a819-0410-b2b0-a2521c9e6caf>
2011-01-21 12:42:33 +0000

    tweak the example

--- examples/weld/weld-se-example1/src/main/java/org/yajul/weldse/example1/SimpleApp.java
+++ examples/weld/weld-se-example1/src/main/java/org/yajul/weldse/example1/SimpleApp.java
@@ -1,7 +1,18 @@
 package org.yajul.weldse.example1;
 
-import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.ShutdownManager;
+import org.jboss.weld.environment.se.StartMain;
 import org.jboss.weld.environment.se.WeldContainer;
+import org.jboss.weld.environment.se.bindings.Parameters;
+import org.jboss.weld.environment.se.events.ContainerInitialized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.List;
 
 /**
  * A simple Weld SE app.
@@ -10,26 +21,39 @@ import org.jboss.weld.environment.se.WeldContainer;
  * Date: 1/20/11
  * Time: 11:19 PM
  */
+@Singleton
 public class SimpleApp
 {
+    private static final Logger log = LoggerFactory.getLogger(SimpleApp.class);
+
+    public SimpleApp()
+    {
+        log.info("Constructor.");
+    }
+
+    private void sayHello()
+    {
+        log.info("Hello world!");
+    }
+
+    public void onStartup(@Observes ContainerInitialized event,@Parameters List<String> commandLine)
+    {
+        log.info("onStartup()");
+    }
+
     public static void main(String[] args)
     {
         try
         {
-            final Weld weld = new Weld();
-            WeldContainer container = weld.initialize();
-            SimpleApp app = container.instance().select(SimpleApp.class).get();
+            WeldContainer container = new StartMain(args).go();
+            final SimpleApp app = container.instance().select(SimpleApp.class).get();
             app.sayHello();
-            weld.shutdown();
+            app.log.info("Shutting down Weld...");
+            container.instance().select(ShutdownManager.class).get().shutdown();
         }
         catch (Throwable t)
         {
             t.printStackTrace();
         }
-    }
-
-    private void sayHello()
-    {
-        System.out.println("Hello world!");
     }
 }
