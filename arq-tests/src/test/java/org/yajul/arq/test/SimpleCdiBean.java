94d4718b3195ffeb858dd3b7c8d89a1fe24b70be
joshuadavis <pgmjsd@gmail.com>
2012-12-31 17:11:53 -0500

    get arquillian embedded glassfish container working

--- arq-tests/src/test/java/org/yajul/arq/test/SimpleCdiBean.java
+++ arq-tests/src/test/java/org/yajul/arq/test/SimpleCdiBean.java
@@ -1,5 +1,7 @@
 package org.yajul.arq.test;
 
+import javax.naming.*;
+
 /**
  * A very simple CDI bean for testing.
  * <br>
@@ -10,5 +12,33 @@ package org.yajul.arq.test;
 public class SimpleCdiBean {
     public void doSomething() {
         System.out.println("hello there");
+        StringBuilder sb = null;
+        try {
+            InitialContext context = new InitialContext();
+            String name = "";
+            sb = new StringBuilder();
+            sb.append("Listing for ").append(name).append("\n");
+            listContext(" ",  context, name, sb);
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sb.toString());
+    }
+
+
+
+    private void listContext(String prefix, Context context, String name, StringBuilder sb)
+            throws NamingException {
+        NamingEnumeration<Binding> bindings = context.listBindings(name);
+        while (bindings.hasMore()) {
+            Binding binding = bindings.next();
+            sb.append(prefix).append(name).append(binding.getName()).append(" -> ")
+                    .append(binding.getClassName()).append("\n");
+            Object obj = binding.getObject();
+            if (obj instanceof Context) {
+                Context nestedContext = (Context) obj;
+                listContext(prefix + binding.getName() + "/",nestedContext,"",sb);
+            }
+        }
     }
 }
