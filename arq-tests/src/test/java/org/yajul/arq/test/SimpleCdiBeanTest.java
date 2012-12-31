91a873f9d973e8517f6be907b9f7b77934a24bba
joshuadavis <pgmjsd@gmail.com>
2012-12-31 15:08:08 -0500

    get arquillian embedded glassfish container working

--- arq-tests/src/test/java/org/yajul/arq/test/SimpleCdiBeanTest.java
+++ arq-tests/src/test/java/org/yajul/arq/test/SimpleCdiBeanTest.java
@@ -1,6 +1,6 @@
 package org.yajul.arq.test;
 
-import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
