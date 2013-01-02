94d180fb347acb685ef09563deb9f30d5f883cbb
joshuadavis <pgmjsd@gmail.com>
2013-01-02 09:57:07 -0500

    JtaTemplate test with Arquillian and embedded glassfish Also... remove more unnecessary IDEA files (they are generated from Maven)

--- core-ee/src/test/java/org/yajul/jta/JtaTest.java
+++ core-ee/src/test/java/org/yajul/jta/JtaTest.java
@@ -49,52 +49,10 @@ public class JtaTest {
                 );
     }
 
-
     @Test
     public void testTransactionTemplates() throws Exception {
-        System.out.println(JndiHelper.listBindings(new DefaultInitialContextProvider().getObject(), ""));
-
-        testBean.check();
-/*
-
-        final JndiLookup lookup = new DefaultJndiLookup();
-        // Use the special JBoss UserTransaction.  Inside EJBS, SessionContext.getUserTransaction() should be used.
-        UserTransaction ut = lookup.lookup(UserTransaction.class, "UserTransaction");
-        showStatus("before");
-        ensureNoTx();
-        new UserTransactionTemplate(ut).doAction(new TxAction());
-        showStatus("after");
-        ensureNoTx();
-        JtaHelper.doInTx(getTxm(),new TxAction());
-        ensureNoTx();
-*/
-    }
-
-    private void ensureNoTx() throws SystemException {
-        assertEquals(Status.STATUS_NO_TRANSACTION,getTxm().getStatus());
-    }
-
-    private void ensureActive() {
-        final TransactionManager txm = getTxm();
-        assertEquals(Status.STATUS_ACTIVE, TransactionStatusHelper.getStatus(txm));
-        assertTrue(TransactionStatusHelper.isInProgress(txm));
-    }
-
-    private void showStatus(String msg) {
-        TransactionManager txm = getTxm();
-        log.info(msg + " : status = " + TransactionStatusHelper.asName(txm));
-    }
-
-    private TransactionManager getTxm() {
-        final JndiLookup lookup = new DefaultJndiLookup();
-        return lookup.lookup(TransactionManager.class, JBossJndiNames.TRANSACTION_MANAGER);
-    }
-
-    private class TxAction implements Callable<Object> {
-        public Object call() throws Exception {
-            showStatus("during");
-            ensureActive();
-            return null;
-        }
+        // UserTransaction is available inside EJBs in all containers.
+        // TransactionManager is not always available.
+        testBean.transactionTemplateCheck();
     }
 }
--- core-ee/src/test/java/org/yajul/jta/JtaTestBean.java
+++ core-ee/src/test/java/org/yajul/jta/JtaTestBean.java
@@ -2,11 +2,16 @@ package org.yajul.jta;
 
 
 import org.yajul.jndi.JndiHelper;
+import org.yajul.util.Callable;
 
+import javax.annotation.Resource;
+import javax.ejb.EJBContext;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
-import javax.naming.InitialContext;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+import java.util.logging.Logger;
 
 /**
  * Test bean for transactions.
@@ -18,8 +23,32 @@ import javax.naming.InitialContext;
 @Stateless
 @TransactionManagement(TransactionManagementType.BEAN)
 public class JtaTestBean {
+    private static final Logger log = Logger.getLogger(JtaTestBean.class.getName());
 
-    public void check() {
-        System.out.println("bindings=" + JndiHelper.listBindings(JndiHelper.getDefaultInitialContext(), ""));
+    @Resource
+    private EJBContext context;
+
+    public void transactionTemplateCheck() throws SystemException {
+        log.info("bindings=" + JndiHelper.listBindings(JndiHelper.getDefaultInitialContext(), ""));
+        if (context == null)
+            throw new IllegalStateException("EJB context is null!");
+        final UserTransaction utx = context.getUserTransaction();
+        if (utx == null)
+            throw new IllegalStateException("UserTransaction is null!");
+
+        if (TransactionStatusHelper.isInProgress(utx.getStatus()))
+            throw new IllegalStateException("UserTransaction is in progress! (1)");
+
+        UserTransactionTemplate template = new UserTransactionTemplate(utx);
+        template.doAction(new Callable<Object>() {
+            public Object call() throws Throwable {
+                if (!TransactionStatusHelper.isInProgress(utx.getStatus()))
+                    throw new IllegalStateException("UserTransaction is NOT in progress!");
+                return null;
+            }
+        });
+
+        if (TransactionStatusHelper.isInProgress(utx.getStatus()))
+            throw new IllegalStateException("UserTransaction is in progress! (2)");
     }
 }
